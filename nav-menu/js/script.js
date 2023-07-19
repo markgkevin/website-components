0694a041f73f44fcdee5dd601ee6016f2bcca573
markgkevin <markymend1001@gmail.com>
2023-07-19 17:32:43 +0800

    javascript for trigger open and close menu

--- nav-menu/js/script.js
+++ nav-menu/js/script.js
@@ -1,9 +1,40 @@
 const navIcon = document.querySelector(".menu-icon");
-// const navMenu = document.querySelector("menu-links");
+const navMenu = document.querySelector(".menu-links");
+const closeBtn = document.querySelector(".close-btn");
+
 
 navIcon.addEventListener("click", () => {
     navIcon.classList.toggle("trigger");
-    // navMenu.classList.toggle("trigger");
-    console.log("working");
-
+    navMenu.classList.toggle("trigger");
 });
+
+closeBtn.addEventListener("click", () => {
+    navMenu.classList.remove("trigger");
+});
+
+document.querySelectorAll(".nav-link")
+    .forEach(n => n.addEventListener("click", () => {
+        navMenu.classList.remove("trigger");
+    }));
+
+
+
+// Refactored codes by ChatGPT for future reference
+
+// const toggleTrigger = () => {
+//     navIcon.classList.toggle("trigger");
+//     navMenu.classList.toggle("trigger");
+// };
+
+// const closeMenu = () => {
+//     navMenu.classList.remove("trigger");
+// };
+
+// const navIcon = document.querySelector(".menu-icon");
+// const navMenu = document.querySelector(".menu-links");
+// const closeBtn = document.querySelector(".close-btn");
+// const navLinks = document.querySelectorAll(".nav-link");
+
+// navIcon.addEventListener("click", toggleTrigger);
+// closeBtn.addEventListener("click", closeMenu);
+// navLinks.forEach(link => link.addEventListener("click", closeMenu));
